7ec34a809e21854b170380ba8bc545de1a37dbe7
DarrenKeig <darren.keig@telent.com>
2022-08-09 10:18:26 +0100

    Removed last bit of fluff

--- core/enums/authentication/app-role.enum.ts
+++
@@ -1,4 +0,0 @@
-// use if you have an api that uses a concept of role based users
-export enum AppRole {
-    AdminUser = 'Administrator',
-}
--- core/enums/notifications/toast-type.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum ToastType {
-    Success,
-    Error,
-}
